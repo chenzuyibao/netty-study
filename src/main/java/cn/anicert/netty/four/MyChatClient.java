0d4f03b27f0ae3628ee0e2cad9a29a4462b1f36f
chenzuyibao <chenzuyibao@163.com>
2019-11-05 18:43:13 +0800

    新增心跳机制示例

---
+++ src/main/java/cn/anicert/netty/four/MyChatClient.java
@@ -0,0 +1,38 @@
+package cn.anicert.netty.four;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class MyChatClient {
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap boostrap = new Bootstrap();
+            boostrap.group(eventLoopGroup).channel(NioSocketChannel.class).handler(new MyChatClientInitializer());
+            /**
+             * 这里和之前例子不一样，他获得的是一个Channel 表示和服务器端连接
+             */
+            Channel channel = boostrap.connect("localhost", 8899).sync().channel();
+
+            BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
+
+            for (;;) {
+                channel.writeAndFlush(bufferReader.readLine() + "\r\n");
+            }
+
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+
+    }
+
+}
