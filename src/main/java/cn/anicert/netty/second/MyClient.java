6d1bc1540d5db52d3fa14e8598e4ee3f320494f1
chenzuyibao <chenzuyibao@163.com>
2019-11-11 10:52:21 +0800

    测试格式

--- src/main/java/cn/anicert/netty/second/MyClient.java
+++ src/main/java/cn/anicert/netty/second/MyClient.java
@@ -7,31 +7,28 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class MyClient {
-    
-    public static void main(String[] args) {
-        
-        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
-        
-        try {
-            /**
-             *  注意
-             *  1.客户端不在是ServerBootstrap而是Bootstrap
-             *  2.反射类不是NioServerSocketChannel而是NioSocketChannel
-             *  3.一般使用handler，而不是用childHandler
-             *  4.不是bind绑定端口而是connect
-             */
-            Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class)
-                .handler(new MyClientInitializer());
-            
-            ChannelFuture channelFuture = bootstrap.connect("localhost", 8899).sync();
-            channelFuture.channel().closeFuture().sync();
-            
-        } catch (Exception e) {
-        } finally {
-            eventLoopGroup.shutdownGracefully();
-        }
-        
-    }
-    
+
+	public static void main(String[] args) {
+
+		EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+
+		try {
+			/**
+			 * 注意 1.客户端不在是ServerBootstrap而是Bootstrap
+			 * 2.反射类不是NioServerSocketChannel而是NioSocketChannel
+			 * 3.一般使用handler，而不是用childHandler. 4.不是bind绑定端口而是connect
+			 */
+			Bootstrap bootstrap = new Bootstrap();
+			bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class).handler(new MyClientInitializer());
+
+			ChannelFuture channelFuture = bootstrap.connect("localhost", 8899).sync();
+			channelFuture.channel().closeFuture().sync();
+
+		} catch (Exception e) {
+		} finally {
+			eventLoopGroup.shutdownGracefully();
+		}
+
+	}
+
 }
--- src/main/java/cn/anicert/netty/second/MyClientInitializer.java
+++ src/main/java/cn/anicert/netty/second/MyClientInitializer.java
@@ -10,18 +10,18 @@ import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
 
 public class MyClientInitializer extends ChannelInitializer<SocketChannel> {
-    
-    @Override
-    protected void initChannel(SocketChannel ch) throws Exception {
-        
-        ChannelPipeline pipeline = ch.pipeline();
-        //解码器
-        pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
-        //编码器
-        pipeline.addLast(new LengthFieldPrepender(4));
-        pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
-        pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
-        pipeline.addLast(new MyClientHandler());
-    }
-    
+
+	@Override
+	protected void initChannel(SocketChannel ch) throws Exception {
+
+		ChannelPipeline pipeline = ch.pipeline();
+		// 解码器
+		pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+		// 编码器
+		pipeline.addLast(new LengthFieldPrepender(4));
+		pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+		pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+		pipeline.addLast(new MyClientHandler());
+	}
+
 }
