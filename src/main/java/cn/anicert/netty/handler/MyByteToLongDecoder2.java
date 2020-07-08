b750113a1b05f202c33fcab27cc24069eea6db16
chenzuyibao <chenzuyibao@163.com>
2020-07-08 20:42:02 +0800

    新增测试代码

---
+++ src/main/java/cn/anicert/netty/handler/MyByteToLongDecoder2.java
@@ -0,0 +1,21 @@
+package cn.anicert.netty.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ReplayingDecoder;
+
+import java.util.List;
+
+/**
+ * @author chenzuyi
+ * @create 2020-07-08 19:57
+ */
+public class MyByteToLongDecoder2 extends ReplayingDecoder<Void> {
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        System.out.println("replaydecoder inovker");
+        out.add(in.readLong());
+    }
+
+}
